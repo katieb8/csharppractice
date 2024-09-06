229204de969c8e51ab88575e9bd1e6092ce4c69b
Katie Burdett <Katie.Burdett@ao.com>
2024-09-06 16:34:33 +0100

    chore: update write line

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,10 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("   /|");
-            Console.WriteLine("  / |");
-            Console.WriteLine(" /  |");
-            Console.WriteLine("/___|");
+            Console.WriteLine("There once was a man named John");
+            Console.WriteLine("He was 80 years old");
+            Console.WriteLine("He really liked the name John");
+            Console.WriteLine("But didn't like being 80");
 
             Console.ReadLine();
         }
